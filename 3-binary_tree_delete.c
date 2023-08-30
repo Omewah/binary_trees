7f9dac171fb3b2792ef3c7b90431ee33d40f91c6
Omewah <joelomewah@ymail.com>
2023-08-30 06:57:25 +0100

    deletes a binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - function that deletes a binary tree
+ * @tree: the tree to be deleted
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
+
+}
